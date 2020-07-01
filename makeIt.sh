e2b6e4f2bd52aac507278a7f221167c36953c8a8
Ammar Ghazzawi <ammar.ghazzawi@protonmail.com>
2020-07-02 01:04:36 +0200

    adding librta submodule

--- makeIt.sh
+++ makeIt.sh
@@ -3,8 +3,11 @@
 init() {
 
   echo "############### INITIALIZATION ###############"
+  # mount the target using nfs
+  sudo mount -t nfs 192.168.1.107:/ /media/nfs
   # define some env variables 
-  export CC=/usr/bin/arm-linux-gnueabihf-g++
+  export CXX=/usr/bin/arm-linux-gnueabihf-g++
+  export CC=/usr/bin/arm-linux-gnueabihf-gcc
   export SYSROOT=/media/nfs
   export EXE_NAME="sarab1"
 
@@ -12,7 +15,7 @@ init() {
   LIB_DIR="${SYSROOT}/usr/local/lib"
   SRC_DIR=$(pwd)
   BUILD_DIR="$(pwd)/build"
-  SQLPARSER_DIR="src/third/sqlparser"
+  LIBRTA_DIR="src/third/librta/src"
   EXE_INSTALL_DIR="${SYSROOT}/tmp"
   
   # create build dir
@@ -20,15 +23,21 @@ init() {
 
 }
 
-build_sqlparser() {
+build_librta() {
 
-  echo "########### BUILDING SQLPARSER #############"
-  cd ${SQLPARSER_DIR}
+  echo "########### BUILDING LIBRTA #############"
+  cd ${LIBRTA_DIR}
   make CXX=${CC}
 
-  if [ $? -eq 0 ]; then
-    cp libsqlparser.so ${LIB_DIR} 
-  fi
+  cd  ${SRC_DIR}
+}
+
+clean_librta() {
+
+  echo "########### CLEANING LIBRTA #############"
+  cd ${LIBRTA_DIR}
+  make clean
+  cd  ${SRC_DIR}
 
 }
 
@@ -39,12 +48,21 @@ build_main() {
   cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain.cmake ../src
   cmake -G "Unix Makefiles" ../src
   cmake --build . --config Debug --target all
-
   echo "############### COPYING EXEs ################"
   if [ $? -eq 0 ]; then
     cp "${BUILD_DIR}/${EXE_NAME}" ${EXE_INSTALL_DIR} 
     cp "${SRC_DIR}/configs/easyloggingpp.conf" ${EXE_INSTALL_DIR}
   fi
+
+  cd  ${SRC_DIR}
+
+}
+
+clean_main() {
+
+  cd ${BUILD_DIR}
+  make clean
+  cd  ${SRC_DIR}
 
 }
 
@@ -52,7 +70,7 @@ build() {
 
   echo "################# BUILDING ##################"
   init
-  build_sqlparser
+  build_librta
   build_main
 
 }
@@ -61,11 +79,8 @@ clean() {
 
   echo "################# CLEANING ##################"
   init
-  cd ${SQLPARSER_DIR}
-  echo ${SQLPARSER_DIR}
-  make clean
-  cd ${BUILD_DIR}
-  make clean
+  clean_librta
+  clean_main
 
 }
 
